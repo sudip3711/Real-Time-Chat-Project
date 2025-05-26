a9a89442986fc194edc8ff9803755050fad1085d
sudip3711 <149355694+sudip3711@users.noreply.github.com>
2025-05-26 15:12:21 +0530

    10

--- Chat_Frontend/src/Services/AxiosHelper.js
+++ Chat_Frontend/src/Services/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL="http://localhost:8080";
+export const baseURL="https://chat-backend-production-adf7.up.railway.app";
 export const httpClient = axios.create({
     baseURL:baseURL
 })
