187f8cb51613e2a47fcaf0232e2021e785225463
sudip3711 <149355694+sudip3711@users.noreply.github.com>
2025-05-27 02:00:48 +0530

    changing_deploy

--- Chat_Frontend/src/Services/AxiosHelper.js
+++ Chat_Frontend/src/Services/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL="https://chat-backend-production-adf7.up.railway.app";
+export const baseURL="https://chat-backend3-2-latest.onrender.com";
 export const httpClient = axios.create({
     baseURL:baseURL
 })
