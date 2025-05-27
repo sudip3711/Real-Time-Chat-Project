746b6ba6c49cd2d77eb8bf098c4fc021f971f894
sudip3711 <149355694+sudip3711@users.noreply.github.com>
2025-05-27 21:08:34 +0530

    fix_env

--- Chat_Frontend/src/Services/AxiosHelper.js
+++ Chat_Frontend/src/Services/AxiosHelper.js
@@ -1,16 +1,16 @@
-// import axios from "axios";
-// export const baseURL="https://chat-backend3-2-latest.onrender.com";
-// export const httpClient = axios.create({
-//     baseURL:baseURL
-// })
-
 import axios from "axios";
-
-// Example condition: use Railway in production, Render otherwise
-export const baseURL = process.env.NODE_ENV === "production"
-  ? "https://chat-backend32-production.up.railway.app"
-  : "https://chat-backend3-2-latest.onrender.com";
-
+export const baseURL="https://chat-backend3-2-latest.onrender.com";
 export const httpClient = axios.create({
-  baseURL: baseURL,
-});
+    baseURL:baseURL
+})
+
+// import axios from "axios";
+
+// // Example condition: use Railway in production, Render otherwise
+// export const baseURL = process.env.NODE_ENV === "production"
+//   ? "https://chat-backend32-production.up.railway.app"
+//   : "https://chat-backend3-2-latest.onrender.com";
+
+// export const httpClient = axios.create({
+//   baseURL: baseURL,
+// });
